4c7a6dba9791768d26be374414ab82d6f2c94fe2
Kirill Matrosov <root@kPC>
2015-11-18 18:18:33 +0300

    refactor tests

--- tests/unit/routes/books-test.js
+++ tests/unit/routes/books-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:books', 'Unit | Route | books', {
+moduleFor('route:authors:author:books', 'Unit | Route | books', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
